6402f146f63a0b34010684b895ab0bdc7d72863c
Jack Mirenzi <jmirenzi717@gmail.com>
2025-11-05 23:11:53 -0500

    fixed pkg setup

--- ros2_ws/src/ur5_draw/setup.py
+++ ros2_ws/src/ur5_draw/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'ur5_draw'
@@ -7,6 +9,8 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        (os.path.join('share', package_name), glob(f'{package_name}/launch/*.launch.py')),
+        (os.path.join('share', package_name), glob(f'{package_name}/scripts/*.py')),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
